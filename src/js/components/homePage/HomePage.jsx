32d331fff925911a0bd4118dd262c9d653b6556f
Uri-Ustrell <uri.ustrell@gmail.com>
2020-05-10 19:03:27 +0200

    <Form />

--- src/js/components/homePage/HomePage.jsx
+++ src/js/components/homePage/HomePage.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import MainWrapper from "../styles/MainWrapper";
-import * as titleActions from "../../state/actions/titleActions";
 
-const HomePage = ({ titles, selectedTitle, changeTitle, getTitles }) => {
+const HomePage = () => {
 	return (
 		<MainWrapper>
 			<Form />
@@ -14,20 +11,4 @@ const HomePage = ({ titles, selectedTitle, changeTitle, getTitles }) => {
 	);
 };
 
-HomePage.propTypes = {
-	titles: PropTypes.array.isRequired,
-	changeTitle: PropTypes.func.isRequired,
-	selectedTitle: PropTypes.string.isRequired,
-};
-
-const mapStateToProps = (state /* , ownProps */) => ({
-	titles: state.title.titles,
-	selectedTitle: state.title.selectedTitle,
-});
-
-const mapDispatchToProps = {
-	changeTitle: titleActions.changeTitle,
-	getTitles: titleActions.getTitles,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default HomePage;
