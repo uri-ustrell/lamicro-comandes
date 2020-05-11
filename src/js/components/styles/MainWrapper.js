1a3afb860db844bfb23af0a7f6ade1167393343f
Uri-Ustrell <uri.ustrell@gmail.com>
2020-05-11 11:21:13 +0200

    app background...

--- src/js/components/styles/MainWrapper.js
+++ src/js/components/styles/MainWrapper.js
@@ -2,8 +2,15 @@ import styled from "styled-components";
 
 const MainWrapper = styled.div`
 	margin: 25px auto;
-	max-width: 800px;
+	max-width: 1200px;
 	padding: 10px;
+	background: rgb(255, 170, 0);
+	background: linear-gradient(
+		0deg,
+		rgba(255, 170, 0, 1) 0%,
+		rgba(255, 249, 236, 1) 83%,
+		rgba(255, 255, 255, 1) 85%
+	);
 `;
 
 export default MainWrapper;
