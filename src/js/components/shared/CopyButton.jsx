6fc263508981dddc5b97715e2f1a7038c8a41cc2
Uri-Ustrell <uri.ustrell@gmail.com>
2020-05-17 00:59:45 +0200

    iPhone alerts

--- src/js/components/shared/CopyButton.jsx
+++ src/js/components/shared/CopyButton.jsx
@@ -22,11 +22,73 @@ const CopyButton = ({ content, available }) => {
 			return;
 		}
 
-		if (navigator.userAgent.match(/ipad|ipod|iphone|mac/i)) {
-			alert("safari", 1);
+		if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+			alert("safari iOS 1");
+			try {
+				navigator.clipboard.writeText(content).then(
+					() => {
+						alert("safari iOS 2");
+						/* Alert user text is copied */
+						setText(copiedText);
+						button.style.backgroundColor = "lightgreen";
+
+						setTimeout(() => {
+							setText(initialText);
+							button.style.backgroundColor = "";
+						}, 3000);
+					},
+					() => {
+						alert(
+							"no es possible copiar el text en aquest dispositiu,\n si us plau feu-ho manualment."
+						);
+					}
+				);
+			} catch (error) {
+				try {
+					alert("safari iOS 3");
+
+					const el = document.createElement("textarea");
+					const editableOld = el.contentEditable;
+					const readOnlyOld = el.readOnly;
+
+					range = document.createRange();
+
+					el.contentEditable = "true";
+					el.readOnly = "false";
+					range.selectNodeContents(el);
+
+					const s = window.getSelection();
+					s.removeAllRanges();
+					s.addRange(range);
+
+					el.setSelectionRange(0, 999999);
+
+					el.contentEditable = editableOld;
+					el.readOnly = readOnlyOld;
+
+					document.execCommand("copy");
+
+					/* Alert user text is copied */
+					setText(copiedText);
+					button.style.backgroundColor = "lightgreen";
+
+					setTimeout(() => {
+						setText(initialText);
+						button.style.backgroundColor = "";
+					}, 3000);
+				} catch (e) {
+					alert(
+						"no es possible copiar el text en aquest dispositiu,\n si us plau feu-ho manualment."
+					);
+				}
+			}
+
+			return;
+		}
+
+		if (navigator.userAgent.match(/macintosh/i)) {
 			navigator.clipboard.writeText(content).then(
 				() => {
-					alert("safari", 2);
 					/* Alert user text is copied */
 					setText(copiedText);
 					button.style.backgroundColor = "lightgreen";
@@ -37,7 +99,6 @@ const CopyButton = ({ content, available }) => {
 					}, 3000);
 				},
 				() => {
-					alert("safari", 3);
 					try {
 						const el = document.createElement("textarea");
 						const editableOld = el.contentEditable;
